fb0adcd11f6bf1ab177757291e658858e808fa24
Grisha Angelov <grisha.angelov89@gmail.com>
2024-08-09 14:15:04 +0200

    Integrate HeadingTitle into the HomePage

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,6 +1,15 @@
+const HeadingTitle = () => (
+  <div className="bg-blue-400 p-1 flex items-center">
+    <img src="/inventory-management.png" width={50} height={50} />
+    <h1 className="text-3xl flex-1 text-center text-blue-950">Inventory Management System</h1>
+  </div>
+)
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <HeadingTitle />
+    </>
   )
 }
 
