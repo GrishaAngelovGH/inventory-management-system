3917b379d6275a0ec1b1985ba2646eb96bd7e5cb
Grisha Angelov <grisha.angelov89@gmail.com>
2024-08-10 10:05:33 +0200

    Integrate IntroductionSection into the HomePage

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -5,10 +5,23 @@ const HeadingTitle = () => (
   </div>
 )
 
+const IntroductionSection = () => (
+  <div className="h-[300px] bg-slate-300 flex">
+    <div className="w-3/6 p-4 text-center flex flex-col justify-center">
+      <h1 className="text-3xl">Effortless Inventory, Seamless Operations—Your Smarter Way to Manage Stock</h1>
+      <p className="text-xl pt-4 text-justify">Transform the way you manage your inventory with our powerful, easy-to-use system. Gain real-time insights, optimize stock levels, and streamline processes—all in one place. Your path to smarter inventory starts here.</p>
+    </div>
+    <div className="w-3/6">
+      <img src="/im-1.jpg" className="w-full h-full opacity-75" />
+    </div>
+  </div>
+)
+
 const HomePage = () => {
   return (
     <>
       <HeadingTitle />
+      <IntroductionSection />
     </>
   )
 }
