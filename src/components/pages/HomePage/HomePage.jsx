483a68d44a77aa9b6b6f077312d3a4846c3e12c3
Grisha Angelov <grisha.angelov89@gmail.com>
2024-08-11 09:45:31 +0200

    Adjust the text size in the sections for the different breakpoints

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -6,10 +6,10 @@ const HeadingTitle = () => (
 )
 
 const IntroductionSection = () => (
-  <div className="h-[300px] bg-slate-300 flex">
+  <div className="md:h-[300px] bg-slate-300 flex">
     <div className="w-3/6 p-4 text-center flex flex-col justify-center">
-      <h1 className="text-3xl">Effortless Inventory, Seamless Operations—Your Smarter Way to Manage Stock</h1>
-      <p className="text-xl pt-4 text-justify">Transform the way you manage your inventory with our powerful, easy-to-use system. Gain real-time insights, optimize stock levels, and streamline processes—all in one place. Your path to smarter inventory starts here.</p>
+      <h1 className="md:text-xl lg:text-3xl">Effortless Inventory, Seamless Operations—Your Smarter Way to Manage Stock</h1>
+      <p className="md:text-xl pt-4 text-justify">Transform the way you manage your inventory with our powerful, easy-to-use system. Gain real-time insights, optimize stock levels, and streamline processes—all in one place. Your path to smarter inventory starts here.</p>
     </div>
     <div className="w-3/6">
       <img src="/im-1.jpg" className="w-full h-full opacity-75" />
@@ -18,9 +18,9 @@ const IntroductionSection = () => (
 )
 
 const DiscoverSection = () => (
-  <div className="h-[300px] bg-[url('/im-2.jpg')] bg-cover flex justify-center items-center">
+  <div className="md:h-[300px] bg-[url('/im-2.jpg')] bg-cover flex justify-center items-center">
     <div className="w-4/6 backdrop-blur-md p-5 rounded-md">
-      <h1 className="text-3xl text-center">Take the First Step Towards Smarter Inventory</h1>
+      <h1 className="text-xl lg:text-3xl text-center">Take the First Step Towards Smarter Inventory</h1>
 
       <p className="mt-5 text-xl text-justify">
         Discover the tools you need to optimize and grow your business.
