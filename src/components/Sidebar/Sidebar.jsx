ca353e00e400f1409908e44e29649ab55ad9ff38
Grisha Angelov <grisha.angelov89@gmail.com>
2024-08-14 13:25:12 +0200

    Add toggle button to the Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,27 @@
-const Sidebar = () => {
+const LeftChevron = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M28 12 L18 24 L28 36" />
+  </svg>
+)
+
+const RightChevron = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M20 12 L30 24 L20 36" />
+  </svg>
+)
+
+const Sidebar = ({ open, onToggle }) => {
   return (
-    <div>Sidebar</div>
+    <div className="h-full bg-slate-600 text-white p-1 text-center relative">
+      <div className="border-2 rounded-md border-blue-100 p-1">Inventory Management</div>
+
+      <div
+        className="w-[40px] h-[40px] text-2xl flex justify-center items-center rounded-full bg-orange-500 hover:bg-orange-600 absolute left-[90%] top-[50%] cursor-pointer"
+        onClick={onToggle}
+      >
+        {open ? <RightChevron /> : <LeftChevron />}
+      </div>
+    </div>
   )
 }
 
--- src/components/Sidebar/Sidebar.test.jsx
+++ src/components/Sidebar/Sidebar.test.jsx
@@ -2,8 +2,14 @@ import { render } from '@testing-library/react'
 
 import Sidebar from './Sidebar'
 
-test('should render Sidebar component', () => {
-  const view = render(<Sidebar />)
+test('should render opened Sidebar component', () => {
+  const view = render(<Sidebar open={true} />)
+
+  expect(view).toMatchSnapshot()
+})
+
+test('should render closed Sidebar component', () => {
+  const view = render(<Sidebar open={false} />)
 
   expect(view).toMatchSnapshot()
 })
