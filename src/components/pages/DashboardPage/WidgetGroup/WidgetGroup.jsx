c9a7b2c4efc524e277b729a19e67ab9dae7e1354
Grisha Angelov <grisha.angelov89@gmail.com>
2024-08-18 09:46:32 +0200

    Integrate TotalProductsWidget into WidgetGroup

--- src/components/pages/DashboardPage/WidgetGroup/WidgetGroup.jsx
+++ src/components/pages/DashboardPage/WidgetGroup/WidgetGroup.jsx
@@ -1,10 +1,12 @@
 import TotalValueWidget from "./TotalValueWidget"
 import TotalCategoriesWidget from "./TotalCategoriesWidget"
+import TotalProductsWidget from "./TotalProductsWidget"
 
 const WidgetGroup = ({ inventory }) => (
   <div className="flex flex-col md:flex-row gap-2 justify-evenly mt-2">
     <TotalValueWidget inventory={inventory} />
     <TotalCategoriesWidget inventory={inventory} />
+    <TotalProductsWidget inventory={inventory} />
   </div>
 )
 
