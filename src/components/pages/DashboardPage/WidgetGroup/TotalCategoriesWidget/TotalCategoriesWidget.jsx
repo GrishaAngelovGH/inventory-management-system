5ac115c3a0d6a6b7d42ec1df8eb74e869d17dcd5
Grisha Angelov <grisha.angelov89@gmail.com>
2024-08-30 09:06:11 +0200

    Reduce the icon size for the widgets in WidgetGroup

--- src/components/pages/DashboardPage/WidgetGroup/TotalCategoriesWidget/TotalCategoriesWidget.jsx
+++ src/components/pages/DashboardPage/WidgetGroup/TotalCategoriesWidget/TotalCategoriesWidget.jsx
@@ -1,7 +1,7 @@
 const TotalCategoriesWidget = ({ inventoryItems }) => (
   <div className="border border-slate-400 rounded-md shadow-lg flex md:w-[250px]">
-    <div className="bg-blue-700 rounded-l-md w-1/2">
-      <img src="/categories-icon.png" className="w-full" />
+    <div className="bg-blue-700 rounded-l-md w-1/2 p-5 flex justify-center items-center">
+      <img src="/categories-icon.png" className="w-2/3" />
     </div>
     <div className="w-1/2 flex flex-col justify-center items-center text-center">
       <div className="text-xl">{inventoryItems.length}</div>
--- src/components/pages/DashboardPage/WidgetGroup/TotalValueWidget/TotalValueWidget.jsx
+++ src/components/pages/DashboardPage/WidgetGroup/TotalValueWidget/TotalValueWidget.jsx
@@ -12,8 +12,8 @@ const TotalValueWidget = ({ inventoryItems }) => {
 
   return (
     <div className="border border-slate-400 rounded-md shadow-lg flex md:w-[250px]">
-      <div className="bg-purple-700 rounded-l-md w-1/2">
-        <img src="/total-value-icon.png" className="w-full" />
+      <div className="bg-purple-700 rounded-l-md w-1/2 p-5 flex justify-center items-center">
+        <img src="/total-value-icon.png" className="w-2/3" />
       </div>
       <div className="w-1/2 flex flex-col justify-center items-center text-center">
         <div className="text-xl">{formatter.format(totalValue)}</div>
