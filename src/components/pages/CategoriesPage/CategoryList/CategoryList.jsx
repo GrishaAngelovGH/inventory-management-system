162b2510f845d9f572957d245c151e09b4389989
Grisha Angelov <grisha.angelov89@gmail.com>
2024-08-31 13:02:37 +0200

    Add a confirmation when deleting a category from the CategoryList

--- src/components/pages/CategoriesPage/CategoryList/CategoryList.jsx
+++ src/components/pages/CategoriesPage/CategoryList/CategoryList.jsx
@@ -1,16 +1,47 @@
+import { useState } from "react"
+
+import Modal from "components/Modal"
+
 import inventory from "persistent/inventory"
 
 const CategoryList = ({ showNotification, onEdit }) => {
+  const [showModal, setShowModal] = useState(false)
+  const [categoryId, setCategoryId] = useState()
+
   const categories = inventory.getItems()
 
   return (
     <div className="border border-slate-400 rounded-md md:w-1/2 max-h-[420px]">
-      <div className="uppercase text-xl text-center border-b-2 border-blue-500 bg-gray-200 rounded-t-md text-gray-500">Category List</div>
+      {
+        showModal && (
+          <Modal
+            title="Delete Category"
+            onConfirm={() => {
+              inventory.deleteCategory(categoryId)
+              showNotification('Successfully deleted category')
+              setShowModal(false)
+            }}
+            onCancel={() => {
+              setCategoryId(null)
+              setShowModal(false)
+            }}
+          >
+            <p className="p-3 text-xl">
+              Are you sure you want to delete this category?
+              All associated products will also be deleted.
+            </p>
+          </Modal>
+        )
+      }
+
+      <div className="uppercase text-xl text-center border-b-2 border-blue-500 bg-gray-200 rounded-t-md text-gray-500">
+        Category List
+      </div>
 
       <div className="max-h-[380px] overflow-auto">
         {
-          categories.map(v => (
-            <div key={v.id} className="mt-2 p-2 text-xl flex justify-between items-center border-b-2 last:border-b-0">
+          categories.map((v, i) => (
+            <div key={v.id} data-testid={`item-${i}`} className="mt-2 p-2 text-xl flex justify-between items-center border-b-2 last:border-b-0">
               <div className="w-[180px] md:w-auto">{v.name}</div>
 
               <div className="flex gap-4">
@@ -22,11 +53,13 @@ const CategoryList = ({ showNotification, onEdit }) => {
                 >
                   <img src="/edit-icon.png" className="w-[20px] h-[20px]" />
                 </button>
+
                 <button
+                  data-testid="delete-button"
                   className="bg-red-500 rounded-md p-2"
                   onClick={() => {
-                    inventory.deleteCategory(v.id)
-                    showNotification('Successfully deleted category')
+                    setCategoryId(v.id)
+                    setShowModal(true)
                   }}
                 >
                   <img src="/delete-icon.png" className="w-[20px] h-[20px]" />
--- src/components/pages/CategoriesPage/CategoryList/CategoryList.test.jsx
+++ src/components/pages/CategoriesPage/CategoryList/CategoryList.test.jsx
@@ -1,9 +1,23 @@
-import { render } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import CategoryList from './CategoryList'
 
-test('should render CategoryList component', () => {
-  const view = render(<CategoryList />)
+describe('CategoryList', () => {
+  test('should render CategoryList component', () => {
+    const view = render(<CategoryList />)
 
-  expect(view).toMatchSnapshot()
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should render confirmation modal when deleting an item from the list', async () => {
+    render(<CategoryList />)
+
+    const firstItem = screen.getByTestId('item-1')
+    const deleteButton = within(firstItem).getByTestId('delete-button')
+
+    await userEvent.click(deleteButton)
+
+    expect(screen.getByText('Are you sure you want to delete this category? All associated products will also be deleted.')).toBeInTheDocument()
+  })
 })
