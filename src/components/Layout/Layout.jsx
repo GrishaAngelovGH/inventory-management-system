862ea4b123ff8bbbf92645c031ce778fd34e5311
Grisha Angelov <grisha.angelov89@gmail.com>
2024-09-13 09:41:16 +0200

    Add subtle gray as a layout's background color

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 const Layout = ({ sidebar, children }) => (
-  <div className="h-screen flex">
+  <div className="h-screen flex bg-gray-100">
     {sidebar}
     <div className="flex-1 overflow-x-auto pl-[2%] p-2">{children}</div>
   </div>
