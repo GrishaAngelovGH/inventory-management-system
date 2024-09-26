d16eb6bd76d3da75e0bc6f22d386fcbc138ffa93
Grisha Angelov <grisha.angelov89@gmail.com>
2024-09-26 13:05:11 +0200

    Integrate flowbite-react component library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+import flowbitePlugin from "flowbite/plugin"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
+    "./node_modules/flowbite/**/*.js",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbitePlugin,
+  ],
 }
