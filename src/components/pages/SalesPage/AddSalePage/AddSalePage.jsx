ff732776dc2207e125ffca2e0a574a3b441b023b
Grisha Angelov <grisha.angelov89@gmail.com>
2024-09-30 15:52:08 +0200

    Implement AddSalePage component able to make new sales for selected product on given date

--- src/components/pages/SalesPage/AddSalePage/AddSalePage.jsx
+++ src/components/pages/SalesPage/AddSalePage/AddSalePage.jsx
@@ -1,10 +1,101 @@
-import SidebarLayout from "components/SidebarLayout"
+import { useState } from "react"
 
-const AddSalePage = () => {
+import SidebarLayout from "components/SidebarLayout"
+import Datepicker from "./Datepicker"
+
+import { Select, TextInput, Label } from "flowbite-react"
+
+import inventory from "persistent/inventory"
+
+const AddSalePage = ({ showNotification }) => {
+  const [productId, setProductId] = useState("")
+  const [productQuantity, setProductQuantity] = useState(1)
+  const [date, setDate] = useState(new Date().toISOString())
+
+  const products = inventory.getAllProducts()
+  const currentProduct = products.find(v => v.id === productId)
+
   return (
     <SidebarLayout>
-      <div className="pl-2 flex flex-col md:flex-row md:justify-center">
-        <div>AddSalePage</div>
+      <div className="pl-2 flex flex-col md:flex-row gap-5">
+        <div className="w-full md:w-1/2">
+          <Select
+            role="combobox"
+            value={productId}
+            onChange={({ target: { value } }) => setProductId(value)}
+          >
+            <option value="">Please select a product</option>
+            {
+              products.map(v => (
+                <option key={v.id} value={v.id}>{v.name}</option>
+              ))
+            }
+          </Select>
+        </div>
+
+        {
+          currentProduct && (
+            <div className="bg-white border border-slate-400 rounded-md md:w-1/2">
+              <div className="uppercase text-xl text-center border-b-2 border-blue-500 bg-gray-200 rounded-t-md text-gray-500">Add new sale</div>
+
+              <div className="p-2 flex flex-col gap-5">
+                <Label>
+                  <div className="text-gray-600 p-1">Product Name</div>
+                  <TextInput
+                    disabled
+                    value={currentProduct.name}
+                  />
+                </Label>
+
+                <Label>
+                  <div className="text-gray-600 p-1">Selling Price</div>
+                  <TextInput
+                    type="number"
+                    disabled
+                    value={currentProduct.sellingPrice}
+                  />
+                </Label>
+
+                <Label>
+                  <div className="text-gray-600 p-1">Product Quantity</div>
+                  <TextInput
+                    data-testid="quantity-input"
+                    type="number"
+                    min="1"
+                    value={productQuantity}
+                    onChange={({ target: { value } }) => setProductQuantity(parseInt(value))}
+                  />
+                </Label>
+                <Label>
+                  <div className="text-gray-600 p-1">Date</div>
+                  <Datepicker value={date} onChange={setDate} />
+                </Label>
+
+                <button
+                  className="bg-blue-500 hover:bg-blue-600 text-white w-[150px] text-xl p-1 rounded-md"
+                  onClick={() => {
+                    try {
+                      inventory.createSale(
+                        currentProduct,
+                        {
+                          quantity: productQuantity,
+                          sellingPrice: currentProduct.sellingPrice,
+                          date: new Date(date).toISOString()
+                        }
+                      )
+
+                      showNotification("The sale is successfully added")
+                    } catch (err) {
+                      showNotification(err.message, true)
+                    }
+                  }}
+                >
+                  Add Sale
+                </button>
+              </div>
+            </div>
+          )
+        }
       </div>
     </SidebarLayout>
   )
--- src/components/pages/SalesPage/AddSalePage/AddSalePage.test.jsx
+++ src/components/pages/SalesPage/AddSalePage/AddSalePage.test.jsx
@@ -1,14 +1,70 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import { MemoryRouter } from 'react-router-dom'
+import { expect, vi } from 'vitest'
+
+vi.mock('persistent/inventory', () => ({
+  default: {
+    getAllProducts: () => [
+      { id: '1', name: 'Smartphone', quantity: 50, sellingPrice: 880 },
+      { id: '2', name: 'Laptop', quantity: 30, sellingPrice: 1320 },
+    ],
+    createSale: vi.fn()
+  }
+}))
+
+import inventory from 'persistent/inventory'
 
 import AddSalePage from './AddSalePage'
 
-test('should render AddSalePage component', () => {
-  const view = render(
-    <MemoryRouter>
-      <AddSalePage />
-    </MemoryRouter>
-  )
+let showNotification
 
-  expect(view).toMatchSnapshot()
+beforeEach(() => {
+  showNotification = vi.fn()
+
+  vi.setSystemTime(new Date('2024-09-30T10:00:00Z'))
+})
+
+afterEach(() => {
+  vi.useRealTimers()
+})
+
+describe('AddSalePage', () => {
+  test('should render AddSalePage component', () => {
+    const view = render(
+      <MemoryRouter>
+        <AddSalePage showNotification={showNotification} />
+      </MemoryRouter>
+    )
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should render create a new sale', async () => {
+    render(
+      <MemoryRouter>
+        <AddSalePage showNotification={showNotification} />
+      </MemoryRouter>
+    )
+
+    const productSelect = screen.getByRole('combobox')
+    await userEvent.selectOptions(productSelect, '1')
+
+    const quantityInput = screen.getByTestId('quantity-input')
+    quantityInput.value = '';
+    await userEvent.type(quantityInput, '10')
+
+    const confirmButton = screen.getByRole('button', { name: /Add Sale/i })
+    await userEvent.click(confirmButton)
+
+    expect(inventory.createSale).toHaveBeenCalledTimes(1)
+    expect(inventory.createSale).toHaveBeenCalledWith(
+      { id: '1', name: 'Smartphone', quantity: 50, sellingPrice: 880 },
+      { date: '2024-09-30T10:00:00.000Z', quantity: 10, sellingPrice: 880 }
+    )
+
+    expect(showNotification).toHaveBeenCalledTimes(1)
+    expect(showNotification).toHaveBeenCalledWith('The sale is successfully added')
+  })
 })
